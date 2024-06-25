07ff5092141e597b8f9cd543d073ad1f8ef5764d
chengensung1219 <156931175+chengensung1219@users.noreply.github.com>
2024-06-25 14:59:28 -0600

    swift file created

---
+++ dali-app/dali-app/dali_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dali_appApp.swift
+//  dali-app
+//
+//  Created by Simon Sung on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct dali_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

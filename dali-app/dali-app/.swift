5825696bbff0aaea701dcee76f36528d7c988c53
sethchens <bryan31032@gmail.com>
2024-09-17 15:10:04 -0600

    Organized working directory

---
+++ dali-app/dali-app/.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  DaliAPP
+//
+//  Created by Simon Sung on 9/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
